f83da1a4dec3364fae77fea2d0e8ecbeb588e5bf
James Loyd <jamestloyd@gmail.com>
2023-10-07 01:07:00 -0400

    one slight change to include more information

--- KriegBot.API/Github/WebhookProcessor.cs
+++ KriegBot.API/Github/WebhookProcessor.cs
@@ -9,6 +9,6 @@ public sealed class WebhookProcessor : WebhookEventProcessor
     protected override Task ProcessPullRequestWebhookAsync(WebhookHeaders headers, PullRequestEvent pullRequestEvent, PullRequestAction action)
     {
         return Task.Run(() => { Console.WriteLine(
-            $"Hello there, this event was raised by {pullRequestEvent.Sender.Name}"); });
+            $"Hello there, this event was raised by {pullRequestEvent.Sender.Name} username? {pullRequestEvent.Sender.Login} action? {action}"); });
     }
 }

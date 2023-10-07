80474feec3ab34fa28264aad0bdba79b2119c2c8
James Loyd <jamestloyd@gmail.com>
2023-10-07 01:25:34 -0400

    ensuring there is json that we can use to go after actions if need be

--- KriegBot.API/Github/WebhookProcessor.cs
+++ KriegBot.API/Github/WebhookProcessor.cs
@@ -8,7 +8,8 @@ public sealed class WebhookProcessor : WebhookEventProcessor
 {
     protected override Task ProcessPullRequestWebhookAsync(WebhookHeaders headers, PullRequestEvent pullRequestEvent, PullRequestAction action)
     {
+        
         return Task.Run(() => { Console.WriteLine(
-            $"Hello there, this event was raised by {pullRequestEvent.Sender.Name} username? {pullRequestEvent.Sender.Login} action? {action}"); });
+            $"Hello there, this event was raised by {pullRequestEvent.Sender.Name} username? {pullRequestEvent.Sender.Login} action? {Newtonsoft.Json.JsonConvert.SerializeObject(action)}"); });
     }
 }

11097288f7c79fb9bdf2f3a06e0797a43ec0a016
James Loyd <jamestloyd@gmail.com>
2023-10-07 01:46:21 -0400

    handled some more cases, now need to dispatch via grpc to the bot

--- KriegBot.API/Github/WebhookProcessor.cs
+++ KriegBot.API/Github/WebhookProcessor.cs
@@ -8,8 +8,26 @@ public sealed class WebhookProcessor : WebhookEventProcessor
 {
     protected override Task ProcessPullRequestWebhookAsync(WebhookHeaders headers, PullRequestEvent pullRequestEvent, PullRequestAction action)
     {
-        
+        switch (action)
+        {
+            case PullRequestActionValue.Opened:
+                Console.WriteLine("PR Opened");
+                break;
+            case PullRequestActionValue.ReadyForReview:
+                Console. WriteLine("Ready for review");
+                break;
+            case PullRequestActionValue.Reopened:
+                Console.WriteLine("Reopened");
+                break;
+            case PullRequestActionValue.Synchronize:
+                Console.WriteLine("Synchronized");
+                break;
+            default:
+                Console.WriteLine("Some other action");
+                break;
+        }
+
         return Task.Run(() => { Console.WriteLine(
-            $"Hello there, this event was raised by {pullRequestEvent.Sender.Name} username? {pullRequestEvent.Sender.Login} action? {Newtonsoft.Json.JsonConvert.SerializeObject(pullRequestEvent)}"); });
+            $"Hello there, this event was raised by {pullRequestEvent.PullRequest.RequestedReviewers} username? {pullRequestEvent.Sender.Login} action? {Newtonsoft.Json.JsonConvert.SerializeObject(pullRequestEvent)}"); });
     }
 }

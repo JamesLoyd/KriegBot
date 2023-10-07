d64e8260845b6e5f08a86cb51322abb60e5ae2b9
James Loyd <jamestloyd@gmail.com>
2023-10-07 00:09:39 -0400

    one more change to how we are gonna test the flow

--- KriegBot.API/Controllers/GithubController.cs
+++ KriegBot.API/Controllers/GithubController.cs
@@ -9,11 +9,10 @@ public class GithubController : ControllerBase
     
     public IActionResult Post(GithubWebhook.GhWebhook webhook)
     {
-        Console.WriteLine(webhook.PayloadObject);
+        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(webhook.PayloadObject));
         if (webhook.Event == GithubWebhook.Events.PullRequestEvent.EventString)
         {
             Console.WriteLine("That's a cool pull request");
-            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(webhook.PayloadObject));
         }
         return Ok();
     }
